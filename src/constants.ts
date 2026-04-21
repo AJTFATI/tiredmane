01f4a876733d5fa272ced5c9934de674fd3583f0
AJT <alex@alexanderjtaylor.com>
2026-04-21 16:26:00 -0400

    shouldaknown (social media)

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
 import IconBrandInstagram from "@/assets/icons/IconBrandInstagram.svg";
 import { SITE } from "@/config";
 
-interface Social {
+export interface Social {
   name: string;
   href: string;
   linkTitle: string;
@@ -38,10 +38,31 @@ export const SOCIALS: Social[] = [
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
- {
+  {
     name: "Instagram",
     href: "https://www.instagram.com/alexanderjordantaylor/",
     linkTitle: `${SITE.title} on Instagram`,
     icon: IconBrandInstagram,
-},
- ]  as const;
+  },
+];
+
+export const SHARE_LINKS: Social[] = [
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/alexanderjordantaylor/",
+    linkTitle: `Share this post on Instagram`,
+    icon: IconBrandInstagram,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/sharing/share-offsite/?url=",
+    linkTitle: `Share this post on LinkedIn`,
+    icon: IconLinkedin,
+  },
+  {
+    name: "Mail",
+    href: "mailto:?subject=See%20this%20post&body=",
+    linkTitle: `Share this post via email`,
+    icon: IconMail,
+  },
+];
