c91aeed8051605a40bada2cc5670d71e1f7b88e3
AJT <alex@alexanderjtaylor.com>
2026-04-21 09:13:22 -0400

    nystateofmind (updating home page)

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,23 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
-  lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 4,
+  website: "https://alexanderjtaylor.com", // replace this with your deployed domain
+  author: "A.J Taylor",
+  profile: "https://alexanderjtaylor.com",
+  desc: "Headlines, Hootsuite, and Hoops. Certified J.A.G's take on culture and social media.",
+  title: "alexanderjtaylor.com",
+  ogImage: "src/assets/images/AstroPaper-v3.png",
+  lightAndDarkMode: false,
+  postPerIndex: 3,
+  postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Edit page",
     url: "https://github.com/satnaing/astro-paper/edit/main/",
   },
-  dynamicOgImage: true,
+  dynamicOgImage: false,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "America/New_York", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
